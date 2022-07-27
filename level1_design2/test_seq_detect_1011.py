c15cf2e13eadb54452e363747bc834457148fce7
Yash-tomar <pine.tomaryash@gmail.com>
2022-07-27 19:53:52 +0000

    testcase with assert

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -27,17 +27,11 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)
 
     cocotb.log.info('#### CTB: Develop your test here! ######')
-    #await Timer(2, units='ns')
-    #yield RisingEdge(dut.inp_bit.value <= 1) 
-    #await Timer(10, units='ns')
-    #dut.inp_bit.value <= 0;
-    #await Timer(10, units='ns')
-
-    #dut.inp_bit.value <= 1;
-    #await Timer(10, units='ns')
-    #dut.inp_bit.value <= 1;
-    #await Timer(10, units='ns')
-    #assert dut.seq_seen == 1, "Seq_check test fails"
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value = 1
+    await RisingEdge(dut.clk)
+    await Timer(10, units='ns')
+    dut._log.info("Inp_bit : %s ,seq_seen : %s, current_state : %s , next_state : %s",str(dut.inp_bit.value),str(dut.seq_seen.value),str(dut.current_state.value),str(dut.next_state.value))
 
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 1
@@ -68,7 +62,8 @@ async def test_seq_bug1(dut):
     await RisingEdge(dut.clk)
     await Timer(10, units='ns')
     dut._log.info("Inp_bit : %s ,seq_seen : %s, current_state : %s , next_state : %s",str(dut.inp_bit.value),str(dut.seq_seen.value),str(dut.current_state.value),str(dut.next_state.value))
-    
+    assert dut.seq_seen==1,"seq_seen not achieve"
+
     await FallingEdge(dut.clk)
     dut.inp_bit.value = 0
     await RisingEdge(dut.clk)
@@ -98,3 +93,4 @@ async def test_seq_bug1(dut):
     await RisingEdge(dut.clk)
     await Timer(10, units='ns')
     dut._log.info("Inp_bit : %s ,seq_seen : %s, current_state : %s , next_state : %s",str(dut.inp_bit.value),str(dut.seq_seen.value),str(dut.current_state.value),str(dut.next_state.value))
+    assert dut.seq_seen==1,"seq_det not achieve"
